93f8d4bb1c4d3cd864857e67d39a5e91611ae959
Sean Lim <seanlim09@outlook.com>
2025-03-18 10:56:31 +0800

    Added Dijkstra implementation with runner code

---
+++ algorithms/dijkstra.py
@@ -0,0 +1,41 @@
+class Graph:
+    def __init__(self, size):
+        self.adj_matrix = [[0] * size for _ in range(size)]
+        self.size = size
+        self.vertex_data = [''] * size
+
+    def add_edge(self, u, v, weight):
+        if 0 <= u < self.size and 0 <= v < self.size:
+            self.adj_matrix[u][v] = weight
+            self.adj_matrix[v][u] = weight
+
+    def add_vertex_data(self, vertex, data):
+        if 0 <= vertex < self.size:
+            self.vertex_data[vertex] = data
+
+    def dijkstra(self, start_vertex_data):
+        start_vertex = self.vertex_data.index(start_vertex_data)
+        distances = [float('inf')] * self.size
+        distances[start_vertex] = 0
+        visited = [False] * self.size
+
+        for _ in range(self.size):
+            min_distance = float('inf')
+            u = None
+            for i in range(self.size):
+                if not visited[i] and distances[i] < min_distance:
+                    min_distance = distances[i]
+                    u = i
+
+            if u is None:
+                break
+
+            visited[u] = True
+
+            for v in range(self.size):
+                if self.adj_matrix[u][v] != 0 and not visited[v]:
+                    alt = distances[u] + self.adj_matrix[u][v]
+                    if alt < distances[v]:
+                        distances[v] = alt
+
+        return distances
---
+++ main.py
@@ -0,0 +1,20 @@
+from algorithms import dijkstra
+
+dij = dijkstra.Graph(5)
+dij.add_vertex_data
+
+dij.add_vertex_data(0, 'A')
+dij.add_vertex_data(1, 'B')
+dij.add_vertex_data(2, 'C')
+dij.add_vertex_data(3, 'D')
+dij.add_vertex_data(4, 'E')
+
+dij.add_edge(0, 1, 1)
+dij.add_edge(0, 2, 2)
+dij.add_edge(0, 4, 9)
+dij.add_edge(1, 3, 5)
+dij.add_edge(2, 3, 3)
+dij.add_edge(3, 4, 2)
+
+distances = dij.dijkstra('A')
+print(distances)
