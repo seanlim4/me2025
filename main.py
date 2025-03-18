79b63729d00917cb04437237df318791d97b90b1
Sean Lim <seanlim09@outlook.com>
2025-03-18 14:26:15 +0800

    implemented timing code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from algorithms import dijkstra, bruteforce
+import timeit
 
-# Dijkstra's Algorithm
+dijSetup = """
+from algorithms import dijkstra
 dij = dijkstra.Graph(5)
 
 dij.add_vertex_data(0, 'A')
@@ -15,8 +16,10 @@ dij.add_edge(0, 4, 9)
 dij.add_edge(1, 3, 5)
 dij.add_edge(2, 3, 3)
 dij.add_edge(3, 4, 2)
+"""
 
-# Brute Force Approach
+bfSetup = """
+from algorithms import bruteforce
 bf = bruteforce.Graph(5)
 
 bf.add_vertex_data(0, 'A')
@@ -31,6 +34,10 @@ bf.add_edge(0, 4, 9)
 bf.add_edge(1, 3, 5)
 bf.add_edge(2, 3, 3)
 bf.add_edge(3, 4, 2)
+"""
 
-print(dij.dijkstra('A'))
-print(bf.bruteforce('A'))
+bfExec = timeit.timeit(stmt="print(bf.bruteforce('A'))", setup=bfSetup, number=1000)
+dijExec = timeit.timeit(stmt="print(dij.dijkstra('A'))", setup=dijSetup, number=1000)
+
+print(f"bfExec runtime: {bfExec * 1000}ms")
+print(f"dijExec runtime: {dijExec * 1000}ms")
