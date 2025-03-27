6cc2b4c509014009b43b0870f9ec0cdaaaa266e6
Sean Lim <seanlim09@outlook.com>
2025-03-27 21:07:55 +0800

    small changes

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import timeit
+repetitions = 100000
 
 dijSetup = """
 from algorithms import dijkstra
-dij = dijkstra.Graph(5)
+dij = dijkstra.Graph(6)
 
 dij.add_vertex_data(0, 'A')
 dij.add_vertex_data(1, 'B')
 dij.add_vertex_data(2, 'C')
 dij.add_vertex_data(3, 'D')
 dij.add_vertex_data(4, 'E')
+dij.add_vertex_data(5, 'F')
 
 dij.add_edge(0, 1, 1)
 dij.add_edge(0, 2, 2)
@@ -16,17 +18,22 @@ dij.add_edge(0, 4, 9)
 dij.add_edge(1, 3, 5)
 dij.add_edge(2, 3, 3)
 dij.add_edge(3, 4, 2)
+dij.add_edge(1, 4, 8)
+dij.add_edge(0, 5, 2)
+dij.add_edge(4, 5, 10)
+dij.add_edge(3, 5, 2)
 """
 
 bfSetup = """
 from algorithms import bruteforce
-bf = bruteforce.Graph(5)
+bf = bruteforce.Graph(6)
 
 bf.add_vertex_data(0, 'A')
 bf.add_vertex_data(1, 'B')
 bf.add_vertex_data(2, 'C')
 bf.add_vertex_data(3, 'D')
 bf.add_vertex_data(4, 'E')
+bf.add_vertex_data(5, 'F')
 
 bf.add_edge(0, 1, 1)
 bf.add_edge(0, 2, 2)
@@ -34,10 +41,14 @@ bf.add_edge(0, 4, 9)
 bf.add_edge(1, 3, 5)
 bf.add_edge(2, 3, 3)
 bf.add_edge(3, 4, 2)
+bf.add_edge(1, 4, 8)
+bf.add_edge(0, 5, 2)
+bf.add_edge(4, 5, 10)
+bf.add_edge(3, 5, 2)
 """
 
-bfExec = timeit.timeit(stmt="print(bf.bruteforce('A'))", setup=bfSetup, number=1000)
-dijExec = timeit.timeit(stmt="print(dij.dijkstra('A'))", setup=dijSetup, number=1000)
+bfExec = timeit.timeit(stmt="print(bf.bruteforce('A'))", setup=bfSetup, number=repetitions)
+dijExec = timeit.timeit(stmt="print(dij.dijkstra('A'))", setup=dijSetup, number=repetitions)
 
-print(f"bfExec runtime: {bfExec * 1000}ms")
-print(f"dijExec runtime: {dijExec * 1000}ms")
+print(f"bfExec runtime: {bfExec * 1000 / repetitions}ms")
+print(f"dijExec runtime: {dijExec * 1000 / repetitions}ms")
